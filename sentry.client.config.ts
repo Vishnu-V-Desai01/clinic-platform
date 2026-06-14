ac1a2efcd563e9c39e85a894498857e56682cf75
Vishnu V Desai <vishnu.v.desai7899@gmail.com>
2026-06-14 22:41:38 +0530

    Initial commit: project skeleton with Next.js 14, Tailwind, shadcn/ui, Sentry, and ESLint

---
+++ sentry.client.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever an error or exception occurs on the client.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://dcd3f3e1e4fe678e6243142ee537336e@o4511564652609536.ingest.us.sentry.io/4511564834537472",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
